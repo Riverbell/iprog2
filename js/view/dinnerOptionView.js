32e865c8dd5aa5fa2dbade265f4d24844be2d3f5
Riverbell <emba93@live.se>
2016-02-11 14:39:40 +0100

    Tog bort onödiga kommentarer

--- js/view/dinnerOptionView.js
+++ js/view/dinnerOptionView.js
@@ -7,8 +7,6 @@ var DinnerOptionView = function (container, model) {
 	var numberOfGuests = container.find("#numberOfGuests");
 	var menuContainer = container.find("#menuTable");
 
-	console.log("hej hopp pris", model.getTotalMenuPrice());
-
 	var loadView = function() {
 		numberOfGuests.html(model.getNumberOfGuests());
 
@@ -18,17 +16,13 @@ var DinnerOptionView = function (container, model) {
 					+ "</tr>";
 		var menu = model.getFullMenu();
 
-		console.log(menu);
-		console.log("yoo");
-
 		for (var i = 0; i < menu.length; i++) {
-			console.log("hej",menu[i].name);
-			var bajs = "<tr>"
+			var dish = "<tr>"
 						+ "<td>" + String(menu[i].name) + "</td>"
 						+ "<td class='right-align'>" + String(model.getDishPrice(menu[i].id)) + "</td>"
 					+ "</tr>";
 
-			dinnerString = dinnerString + bajs;
+			dinnerString = dinnerString + dish;
 		}
 
 		dinnerString = dinnerString + "<tr>"
